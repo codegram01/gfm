fc9f245c75aea02c37ce9b8dba9deefa5481a549
codegram01 <wuyen.mail@gmail.com>
2024-08-03 03:51:48 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	// to read arg by flag, we must parse first
+	flag.Parse()
+
+	err := execCommand()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func execCommand() error {
+	command := getArg(0, "Enter command")
+	switch command {
+	case "ls": // list file
+		files, err := os.ReadDir(".")
+		if err != nil {
+			return err
+		}
+		for _, file := range files {
+			fmt.Printf("%s  ", file.Name())
+		}
+		fmt.Println()
+	case "rm": // delete file
+		path := getArg(1, "Enter path")
+		return os.RemoveAll(path)
+	case "pwd": // current path
+		currentPath, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		fmt.Println(currentPath)
+	case "mv": // move file 
+		path1 := getArg(1, "Enter path 1")
+		path2 := getArg(2, "Enter path 2")
+
+		return os.Rename(path1, path2)
+	case "mkdir":
+		path := getArg(1, "Enter Dir")
+		return os.MkdirAll(path, 0777)
+	case "cat":
+		path := getArg(1, "Enter path")
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		os.Stdout.Write(data)
+	case "touch":
+		path := getArg(1, "Enter path")
+		
+		_, err := os.Create(path)
+		return err
+	case "cp":
+		// path old file
+		path1 := getArg(1, "Enter path 1")
+		// path new file
+		path2 := getArg(2, "Enter path 2")
+
+		data, err := os.ReadFile(path1)
+		if err != nil {
+			return err
+		}
+		return os.WriteFile(path2, data, 0777)
+	case "echo":
+		content := getArg(1, "Enter content")
+		path := getArg(2, "Enter path")
+
+
+		return os.WriteFile(path, []byte(content), 0777)
+	}
+
+
+	return nil
+}
+
+// this function must get arg
+// if user not enter arg , it will break and show error
+func getArg(indexArg int, mess string) string {
+	val := flag.Arg(indexArg)
+	if val == "" {
+		log.Fatal(mess)
+	}
+
+	return val
+}
